34b8a27cb7cb55f6f9960e62ba720f0ecd08ccda
thsaravana <th.saravana@gmail.com>
2021-09-06 20:09:45 +0530

    Covariance and contravariance in T

--- src/main/kotlin/com/madrapps/playground/generics/Generics.kt
+++ src/main/kotlin/com/madrapps/playground/generics/Generics.kt
@@ -1,20 +1,81 @@
 package com.madrapps.playground.generics
 
-private interface Vehicle
+private interface Vehicle : Drivable, Sailable, Flyable
 private interface Drivable
 private interface Sailable
 private interface Flyable
 
-private class Car : Vehicle, Drivable
-private class Ship : Vehicle, Sailable
-private class Jet : Vehicle, Flyable
-private class BatMobile: Vehicle, Drivable, Sailable, Flyable
+private class Car : Vehicle
 
-private class Road<T> {}
-private class River<T : Vehicle>
-private class Underground<out T>
-private class Underwater<out T : Vehicle>
-private class Space<in T>
-private class Rails<in T : Vehicle>
-private class Sky<out T> where T : Vehicle, T : Flyable
-private class Sea<in T, out U> where T : Vehicle, T : Sailable, U: Vehicle
+// TODO Without variance
+/*
+private interface Land<T> {
+    fun get(): T
+    fun set(t: T)
+}
+
+private class Road : Land<Drivable> {
+    private lateinit var drivable: Drivable
+
+    override fun get(): Drivable {
+        return drivable
+    }
+
+    override fun set(t: Drivable) {
+        drivable = t
+    }
+}
+
+fun main() {
+    val road: Land<Drivable> = Road()
+    road.set(Car())
+    road.get()
+//    val rail: Land<Vehicle> = road // Not possible
+}
+*/
+
+// TODO Contravariance
+/*
+private interface Land<in T> {
+//    fun get(): T // Not Possible
+    fun set(t: T)
+}
+
+private class Road : Land<Drivable> {
+    private lateinit var drivable: Drivable
+
+    override fun set(t: Drivable) {
+        drivable = t
+    }
+}
+
+fun main() {
+    val road: Land<Drivable> = Road()
+    road.set(Car())
+    val rail: Land<Vehicle> = road
+//    val d: Land<Drivable> = rail // Not possible
+}
+*/
+
+// TODO Covariance
+/*
+private interface Land<out T> {
+    fun get(): T
+//    fun set(t: T) // Not Possible
+}
+
+private class Road : Land<Vehicle> {
+   private lateinit var vehicle: Vehicle
+
+    override fun get(): Vehicle {
+        return vehicle
+    }
+}
+
+fun main() {
+    val road: Land<Vehicle> = Road()
+    road.get()
+    val rail: Land<Drivable> = road
+//    val d: Land<Vehicle> = rail // Not possible
+}
+*/
