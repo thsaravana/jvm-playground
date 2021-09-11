85072eb00965f763a33dc7d720649d14e5c07c69
thsaravana <th.saravana@gmail.com>
2021-09-11 19:35:26 +0530

    Generics - Use site and Declaration site

--- src/main/kotlin/com/madrapps/playground/generics/Generics.kt
+++ src/main/kotlin/com/madrapps/playground/generics/Generics.kt
@@ -1,81 +1,84 @@
 package com.madrapps.playground.generics
 
-private interface Vehicle : Drivable, Sailable, Flyable
-private interface Drivable
-private interface Sailable
-private interface Flyable
+private interface Animal
+private interface Walkable
 
-private class Car : Vehicle
+private class Cat: Animal
 
-// TODO Without variance
-/*
-private interface Land<T> {
-    fun get(): T
-    fun set(t: T)
-}
+private class InVariant {
 
-private class Road : Land<Drivable> {
-    private lateinit var drivable: Drivable
-
-    override fun get(): Drivable {
-        return drivable
+    class Jungle<T>(private var t: T) {
+        fun obtainType(): T {
+            return t
+        }
     }
 
-    override fun set(t: Drivable) {
-        drivable = t
+    fun main() {
+        val jungle: Jungle<Animal> = Jungle(Cat())
+//        val jungleError: Jungle<Any> = jungle // Compile error
     }
 }
 
-fun main() {
-    val road: Land<Drivable> = Road()
-    road.set(Car())
-    road.get()
-//    val rail: Land<Vehicle> = road // Not possible
-}
-*/
+private class DeclarationSite {
 
-// TODO Contravariance
-/*
-private interface Land<in T> {
-//    fun get(): T // Not Possible
-    fun set(t: T)
-}
+    private class CoVariant {
 
-private class Road : Land<Drivable> {
-    private lateinit var drivable: Drivable
+        class Jungle<out T>(val t: T) {
+            fun obtainType(): T {
+                return t
+            }
+            // fun insertType(t: T) {} // Not Possible (Type parameter T is declared as 'out', so no Write operation on type T)
+        }
 
-    override fun set(t: Drivable) {
-        drivable = t
+        fun main() {
+            val jungleWithAnimals: Jungle<Animal> = Jungle(Cat())
+            val jungleWithAnything: Jungle<Any> = jungleWithAnimals // This won't be possible if you remove the `out`
+        }
+    }
+
+    private class ContraVariant {
+
+        class Jungle<in T>(private var t: T) {
+//            fun obtainType(): T { // Not Possible (Type parameter T is declared as 'int', so no Read operation on type T)
+//                return t
+//            }
+             fun insertType(t: T) {}
+        }
+
+        fun main() {
+            val jungleWithAnimals: Jungle<Any> = Jungle(Cat())
+            val jungleWithAnything: Jungle<Animal> = jungleWithAnimals // This won't be possible if you remove the `out`
+        }
     }
 }
 
-fun main() {
-    val road: Land<Drivable> = Road()
-    road.set(Car())
-    val rail: Land<Vehicle> = road
-//    val d: Land<Drivable> = rail // Not possible
-}
-*/
+private class UseSite {
 
-// TODO Covariance
-/*
-private interface Land<out T> {
-    fun get(): T
-//    fun set(t: T) // Not Possible
-}
+    class Jungle<T>(private var t: T) {
+        fun obtainType(): T {
+            return t
+        }
+        fun insertType(t: T) {}
+    }
 
-private class Road : Land<Vehicle> {
-   private lateinit var vehicle: Vehicle
+    private class CoVariant {
 
-    override fun get(): Vehicle {
-        return vehicle
+        fun main() {
+            val jungleWithAnimals: Jungle<Animal> = Jungle(Cat())
+            val jungleWithAnything: Jungle<out Any> = jungleWithAnimals
+//    val type: Animal = jungleWithAnything.obtainType() // Not possible. You can only Read 'Any'
+            val type: Any = jungleWithAnything.obtainType()
+//    jungleWithAnything.insertType(Cat()) // Not possible. You can't Write.
+        }
+    }
+
+    private class ContraVariant {
+
+        fun main() {
+            val jungleWithAnimals: Jungle<Any> = Jungle(Cat())
+            val jungleWithAnything: Jungle<in Animal> = jungleWithAnimals
+            val type: Any? = jungleWithAnything.obtainType()
+            jungleWithAnything.insertType(Cat())
+        }
     }
 }
-
-fun main() {
-    val road: Land<Vehicle> = Road()
-    road.get()
-    val rail: Land<Drivable> = road
-//    val d: Land<Vehicle> = rail // Not possible
-}
-*/
